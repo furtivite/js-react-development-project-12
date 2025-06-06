fdef7df5a87d2d590680c7c98f97b7c8a5eddda3
Egor Levchenko <antytoto@gmail.com>
2025-06-06 12:29:25 +0300

    feat(frontend): add 404 NotFound page and catch-all route

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import HomePage from '../pages/HomePage';
 import LoginPage from '../pages/LoginPage';
+import NotFoundPage from '../pages/NotFound';
 
 export default function App() {
     return (
@@ -11,6 +12,7 @@ export default function App() {
                 <Routes>
                     <Route path="/" element={<HomePage />} />
                     <Route path="/login" element={<LoginPage />} />
+                    <Route path="*" element={<NotFoundPage />} />
                 </Routes>
             </Container>
         </BrowserRouter>
